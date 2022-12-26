d9c4fb4c8ed1d3c9dffdf1986ba029d1a16ad266
Felipe <41008398+StrawberryMaster@users.noreply.github.com>
2022-12-26 16:56:41 -0300

    ESLint instead of JSHint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2022": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+    }
+}
